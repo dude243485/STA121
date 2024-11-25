67e805deb7849f14b83ab601b26f9c8b19aaf296
dude243485 <dude243485@stu.ui.ed.ng>
2024-11-25 23:02:58 +0100

    remove unwanted files

--- project1.r
+++
@@ -1,9 +0,0 @@
-counts <- c(23,17,35,50)
-categories <- c("A", "B", "C", "D")
-
-#create a bar chart
-barplot(counts, names.arg = categories, 
-col = "skyblue", 
-main = "Bar chart example", 
-xlab = "categories", 
-ylab = "Counts")
--- test 1.R
+++
@@ -1,4 +0,0 @@
-setwd("C/Users/user/Desktop/sta 121")
-data <- read.csv("registered students.csv")
-sample_size <- round(0.1  * now(data))
-print(sample_size)
